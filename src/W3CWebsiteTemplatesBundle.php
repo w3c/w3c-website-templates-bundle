262f947e723f9e07c3d9fe3ecbe39687134f82d2
Jean-Guilhem Rouel <jean-gui@w3.org>
2021-05-12 19:21:29 +0200

    Use structure recommended on https://symfony.com/doc/current/bundles/best_practices.html#directory-structure

--- src/W3CWebsiteTemplatesBundle.php
+++ src/W3CWebsiteTemplatesBundle.php
@@ -3,8 +3,12 @@
 namespace W3C\WebsiteTemplatesBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use function dirname;
 
 class W3CWebsiteTemplatesBundle extends Bundle
 {
-
+    public function getPath(): string
+    {
+        return dirname(__DIR__);
+    }
 }
