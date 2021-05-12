afac46e4f55ee885e2985835665e314c742a5b2d
Jean-Guilhem Rouel <jean-gui@w3.org>
2021-05-12 17:59:39 +0200

    Initial commit

---
+++ LICENSE.md
@@ -0,0 +1,3 @@
+All files in this Repository are licensed under the
+[W3C Software and Document License](https://www.w3.org/Consortium/Legal/copyright-software).
+
---
+++ W3CWebsiteTemplatesBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace W3C\WebsiteTemplatesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class W3CWebsiteTemplatesBundle extends Bundle
+{
+
+}
