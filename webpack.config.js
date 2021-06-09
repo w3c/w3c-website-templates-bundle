73be06cf6a70e29b430fb7e95456a67cdde6b937
Marie Manandise <marie@studio24.net>
2021-06-09 15:05:28 +0100

    reorganised assets folders to avoid shipping src files to app when installing bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 module.exports = {
 	entry: {
-		'/public/dist/assets/js/s24-autocomplete': './public/apollo/assets/js/s24-autocomplete.js',
-		'/public/dist/assets/js/comments': './public/apollo/assets/js/comments.js',
-		'/public/dist/assets/js/main': './public/apollo/assets/js/main.js'
+		'/public/dist/assets/js/s24-autocomplete': './assets-src/js/s24-autocomplete.js',
+		'/public/dist/assets/js/comments': './assets-src/js/comments.js',
+		'/public/dist/assets/js/main': './assets-src/js/main.js'
 	},
 	module: {
 		rules: [
