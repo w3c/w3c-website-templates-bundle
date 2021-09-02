f8d471d43d22f3e20ef34a28dbae2949f5d1ec90
Nicola Saunders <nicola@studio24.net>
2021-09-02 17:26:39 +0100

    Correct paths

--- assets-src/js/main/collapsibles.js
+++ assets-src/js/main/collapsibles.js
@@ -20,7 +20,7 @@ var collapsibles = (function () {
 			headingsArray.forEach(function (heading, index) {
 
 				// Insert a button for opening/closing the collapsible section
-				heading.innerHTML = '<button class="button--ghost" aria-expanded="false">' + heading.innerHTML + '<svg xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 320 512" class="icon icon--larger" focusable="false" aria-hidden="true" width="30px" height="30px"><use class="angle-down" href="dist/assets/svg/nav-icons.svg#angle-down"></use><use class="angle-up" href="dist/assets/svg/nav-icons.svg#angle-up"></use></svg></button>';
+				heading.innerHTML = '<button class="button--ghost" aria-expanded="false">' + heading.innerHTML + '<svg xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 320 512" class="icon icon--larger" focusable="false" aria-hidden="true" width="30px" height="30px"><use class="angle-down" href="/dist/assets/svg/nav-icons.svg#angle-down"></use><use class="angle-up" href="/dist/assets/svg/nav-icons.svg#angle-up"></use></svg></button>';
 
 				// Add appropriate aria role to the collapsible section
 				heading.nextElementSibling.setAttribute('aria-hidden', 'true');
