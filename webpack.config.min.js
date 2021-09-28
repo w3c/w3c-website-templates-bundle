b860dd480d6ddafc64b609bdc307536fb070d0a3
Marie Manandise <marie@studio24.net>
2021-09-28 15:55:10 +0100

    fixed build tools - but now have vulnerabilities to fix

--- webpack.config.min.js
+++ webpack.config.min.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
 	entry: {
-		'/public/dist/assets/js/s24-autocomplete': './assets-src/js/s24-autocomplete.js',
+		'/public/dist/assets/js/libraries-extensions/s24-autocomplete': './assets-src/js/libraries-extensions/s24-autocomplete.js',
 		'/public/dist/assets/js/comments': './assets-src/js/comments.js',
 		'/public/dist/assets/js/main': './assets-src/js/main.js'
 	},
