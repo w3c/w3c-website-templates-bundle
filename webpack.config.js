2a714b38cddea1dd85975c99790a4faca70f7938
Nicola Saunders <nicola@studio24.net>
2021-09-30 11:10:27 +0100

    Update webpack config with missing items

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ module.exports = {
 	entry: {
 		'/public/dist/assets/js/libraries-extensions/s24-autocomplete': './assets-src/js/libraries-extensions/s24-autocomplete.js',
 		'/public/dist/assets/js/comments': './assets-src/js/comments.js',
-		'/public/dist/assets/js/main': './assets-src/js/main.js'
+		'/public/dist/assets/js/main': './assets-src/js/main.js',
+		'/public/dist/assets/js/country-autocomplete': './assets-src/js/country-autocomplete.js',
+		'/public/dist/assets/js/slider': './assets-src/js/slider.js',
+		'/public/dist/assets/js/working-group-autocomplete': './assets-src/js/working-group-autocomplete.js'
 	},
 	module: {
 		rules: [
