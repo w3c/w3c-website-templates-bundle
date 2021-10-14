3e438935ed4aa2140c89abee41fdaec00a4414df
Marie Manandise <marie@studio24.net>
2021-10-14 10:39:08 +0100

    members component - randomising logos

--- assets-src/js/members.js
+++ assets-src/js/members.js
@@ -1,5 +1,6 @@
 let membersPick = (function (){
     const membersContainer = document.querySelector('.component--members__grid');
+    const updatedMembersContainer = membersContainer.cloneNode(false);
 
     let shuffledMembers = membersData
         .map((value) => ({ value, sort: Math.random() }))
@@ -8,13 +9,14 @@ let membersPick = (function (){
 
     let pickedMembers = shuffledMembers.slice(0,8);
 
-    membersContainer.innerHTML = '';
 
     for (let membersIndex = 0; membersIndex < pickedMembers.length; membersIndex++) {
         let memberDiv = document.createElement('div');
         let membersLogoHref = (pickedMembers[membersIndex].logo !== null)? pickedMembers[membersIndex].logo : 'https://via.placeholder.com/150';
         memberDiv.innerHTML = '<img src="' + membersLogoHref + '" alt="' + pickedMembers[membersIndex].name + '" loading="lazy" />';
-        membersContainer.appendChild(memberDiv);
+        updatedMembersContainer.appendChild(memberDiv);
     }
+
+    membersContainer.parentNode.replaceChild(updatedMembersContainer, membersContainer);
 
 })();
