cbd491956c55f6a4107ec089cf09d5329be98152
Marie Manandise <marie@studio24.net>
2021-10-14 16:07:34 +0100

    text direction in content slider and tweaks to members JS script

--- assets-src/js/members.js
+++ assets-src/js/members.js
@@ -1,18 +1,18 @@
 let membersPick = (function (){
-    const membersContainer = document.querySelector('.component--members__grid');
-    const updatedMembersContainer = membersContainer.cloneNode(false);
+    var membersContainer = document.querySelector('.component--members__grid');
+    var updatedMembersContainer = membersContainer.cloneNode(false);
 
     let shuffledMembers = membersData
         .map((value) => ({ value, sort: Math.random() }))
         .sort((a, b) => a.sort - b.sort)
         .map(({ value }) => value);
 
-    let pickedMembers = shuffledMembers.slice(0,8);
+    var pickedMembers = shuffledMembers.slice(0,8);
 
 
-    for (let membersIndex = 0; membersIndex < pickedMembers.length; membersIndex++) {
-        let memberDiv = document.createElement('div');
-        let membersLogoHref = (pickedMembers[membersIndex].logo !== null)? pickedMembers[membersIndex].logo : 'https://via.placeholder.com/150';
+    for (var membersIndex = 0; membersIndex < pickedMembers.length; membersIndex++) {
+        var memberDiv = document.createElement('div');
+        var membersLogoHref = (pickedMembers[membersIndex].logo !== null)? pickedMembers[membersIndex].logo : 'https://via.placeholder.com/150';
         memberDiv.innerHTML = '<img src="' + membersLogoHref + '" alt="' + pickedMembers[membersIndex].name + '" loading="lazy" />';
         updatedMembersContainer.appendChild(memberDiv);
     }
