6d54911e6e80a4e5eaa62c33e4319fdfa58d89de
Jean-Guilhem Rouel <jean-gui@w3.org>
2021-11-05 18:27:16 +0100

    translations use urls absolute_url filter is not needed anymore

--- src/Twig/TwigExtension.php
+++ src/Twig/TwigExtension.php
@@ -55,25 +55,6 @@ class TwigExtension extends AbstractExtension
         ];
     }
 
-    public function absoluteUrl(string $url, string $language = 'en'): string
-    {
-        // $url is already absolute, don't change it
-        if (preg_match(';^(http://|https://);', $url)) {
-            return $url;
-        }
-
-        if (strpos($url, '/') !== 0) {
-            $url = '/' . $url;
-        }
-
-        $prefix = '';
-        if ($language !== 'en') {
-            $prefix = '/' . $language;
-        }
-
-        return $prefix . $url;
-    }
-
     public function pregReplace(string $subject, string $pattern, string $replacement, int $limit = -1): string
     {
         return preg_replace($pattern, $replacement, $subject, $limit);
