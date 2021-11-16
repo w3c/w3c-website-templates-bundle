c6757f51bb68fe18b16b69c0119be1174250bb50
Nicola Saunders <nicola@studio24.net>
2021-11-16 14:24:57 +0000

    Replace var with let

--- assets-src/js/members.js
+++ assets-src/js/members.js
@@ -1,18 +1,18 @@
 let membersPick = (function (){
-    var membersContainer = document.querySelector('.component--members__grid');
-    var updatedMembersContainer = membersContainer.cloneNode(false);
+    let membersContainer = document.querySelector('.component--members__grid');
+    let updatedMembersContainer = membersContainer.cloneNode(false);
 
     let shuffledMembers = membersData
         .map((value) => ({ value, sort: Math.random() }))
         .sort((a, b) => a.sort - b.sort)
         .map(({ value }) => value);
 
-    var pickedMembers = shuffledMembers.slice(0,6);
+    let pickedMembers = shuffledMembers.slice(0,6);
 
-		for (var membersIndex = 0; membersIndex < pickedMembers.length; membersIndex++) {
-			var memberWrap = document.createElement('figure');
+		for (let membersIndex = 0; membersIndex < pickedMembers.length; membersIndex++) {
+			let memberWrap = document.createElement('figure');
 			if (pickedMembers[membersIndex].logo !== null) {
-				var membersLogoHref = pickedMembers[membersIndex].logo;
+				let membersLogoHref = pickedMembers[membersIndex].logo;
 				memberWrap.innerHTML = '<div class="l-box l-box--no-border"><img src="' + membersLogoHref + '" alt="' + pickedMembers[membersIndex].name + '" loading="lazy" /></div>';
 			} else {
 				memberWrap.innerHTML = '<figcaption>' + pickedMembers[membersIndex].name + '</figcaption>';
