4aeef39c07bce9a8c902ecdc821b7aa3b1c80129
Jean-Guilhem Rouel <jean-gui@w3.org>
2021-12-13 16:01:40 +0100

    add error templates

--- src/DependencyInjection/W3CWebsiteTemplatesExtension.php
+++ src/DependencyInjection/W3CWebsiteTemplatesExtension.php
@@ -5,10 +5,11 @@ namespace W3C\WebsiteTemplatesBundle\DependencyInjection;
 use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class W3CWebsiteTemplatesExtension extends Extension
+class W3CWebsiteTemplatesExtension extends Extension implements PrependExtensionInterface
 {
 
     /**
@@ -21,5 +22,17 @@ class W3CWebsiteTemplatesExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.yaml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        // I recommend using FileLocator here
+        $thirdPartyBundlesViewFileLocator = (new FileLocator(__DIR__ . '/../../templates/bundles'));
+
+        $container->loadFromExtension('twig', [
+            'paths' => [
+                $thirdPartyBundlesViewFileLocator->locate('TwigBundle') => 'Twig',
+            ],
+        ]);
     }
 }
