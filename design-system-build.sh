fd8ba28a866040688e08c17cf34fd0ed0e3357b4
Jean-Guilhem Rouel <jean-gui@w3.org>
2022-12-14 14:23:14 +0100

    fix path to assets

--- design-system-build.sh
+++ design-system-build.sh
@@ -2,7 +2,7 @@
 
 # Run build commands
 mkdir -p _dist/dist/assets
-cp -r public/dist/assets/ _dist/dist/assets
+cp -r public/dist/assets/ _dist/dist/
 
 # Copy favicon
 cp public/favicon.ico _dist/favicon.ico
