54b0f51a44c9c8b2dacfaa9d82b7f71b1a49b5a3
Jean-Guilhem Rouel <jean-gui@w3.org>
2023-02-24 16:49:43 +0100

    add twig extension to retrieve menus from http requests

---
+++ src/Extensions/Twig/GetFromFrontendExtension.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace W3C\WebsiteTemplatesBundle\Extensions\Twig;
+
+use Symfony\Component\HttpClient\CachingHttpClient;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpCache\Store;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Throwable;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class GetFromFrontendExtension extends AbstractExtension
+{
+    private string $baseUrl;
+    private HttpClientInterface $client;
+
+    public function __construct(string $baseUrl, HttpClientInterface $client, string $cachePath)
+    {
+        $this->baseUrl = $baseUrl;
+        $store  = new Store($cachePath);
+        $this->client = new CachingHttpClient($client, $store, ['default_ttl' => 86400]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('global_nav', [$this, 'globalNav']),
+            new TwigFunction('lang_nav', [$this, 'langNav'])
+        ];
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return string
+     */
+    private function fromFrontend(string $path): string
+    {
+        $content = '';
+        try {
+            $response = $this->client->request('GET', $this->baseUrl . $path);
+
+            $statusCode = $response->getStatusCode();
+
+            if ($statusCode === Response::HTTP_OK) {
+                $content = $response->getContent();
+            }
+        } catch (Throwable $e) {}
+
+        return $content;
+    }
+
+    public function globalNav(): string
+    {
+        return $this->fromFrontend('/global-nav/');
+    }
+
+    public function langNav(): string
+    {
+        return $this->fromFrontend('/lang-nav/');
+    }
+}
+
