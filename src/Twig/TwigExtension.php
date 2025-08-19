ce3622b0e061c0b3aa0f93517faf191272a7a36e
Nicola Saunders <nicola@studio24.net>
2025-08-19 11:12:52 +0100

    Add Twig filter to format CKEditor table markup
    - Replace `<figure>` with div needed for responsive tables
    - Replace `<figcaption>` with proper table `<caption>`

--- src/Twig/TwigExtension.php
+++ src/Twig/TwigExtension.php
@@ -7,6 +7,7 @@ use DateTimeImmutable;
 use DateTimeInterface;
 use DateTimeZone;
 use Exception;
+use Masterminds\HTML5;
 use SimpleXMLElement;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Translation\IdentityTranslator;
@@ -73,7 +74,8 @@ class TwigExtension extends AbstractExtension
             new TwigFilter('locale_to_bcp47', [$this, 'localeToBcp47']),
             new TwigFilter('bcp47_to_locale', [$this, 'bcp47ToLocale']),
             new TwigFilter('obfuscate', [$this, 'obfuscate'], ['is_safe' => ['html']]),
-            new TwigFilter('i_to_em', [$this, 'iToEm'])
+            new TwigFilter('i_to_em', [$this, 'iToEm']),
+            new TwigFilter('formatTable', [$this, 'formatTable']),
         ];
     }
 
@@ -312,5 +314,78 @@ class TwigExtension extends AbstractExtension
         ], $text);
     }
 
+    public function formatTable($content)
+    {
+        $html5 = new HTML5();
 
+        // Wrap the incoming snippet so we can safely query & replace
+        $dom =  $html5->loadHTML(
+            '<!DOCTYPE html><meta charset="utf-8"><div id="__root__">'.$content.'</div>'
+        );
+
+        // Find our temporary root
+        $xpath = new \DOMXPath($dom);
+        $root  = $xpath->query('//*[@id="__root__"]')->item(0);
+
+        // Collect <figure> nodes into an array (DOMNodeList is live)
+        $figures = [];
+        foreach ($dom->getElementsByTagName('figure') as $figure) {
+            $figures[] = $figure;
+        }
+
+        foreach ($figures as $figure) {
+            $table = $figure->getElementsByTagName('table')->item(0);
+
+            if (!$table) {
+                continue; // Skip if there's no <table> in the <figure>
+            }
+
+            // Create a <div> with the required attributes to wrap the <table>
+            $div = $dom->createElement('div');
+            $div->setAttribute('role', 'region');
+            $div->setAttribute('tabindex', '0');
+            $div->setAttribute('class', 'table-wrap');
+
+            // If a caption has been entered, move this out of <figcaption> (which can be removed) and into <caption>, which should be the first child element of <table>
+            // Also set a globally-unique id on <caption>
+            $figcaption = $figure->getElementsByTagName('figcaption')->item(0);
+            if ($figcaption) {
+                $captionContent = $figcaption->textContent;
+
+                // Generate globally unique strong ID, with safe fallback
+                try {
+                    $captionId = 'table-caption-'.bin2hex(random_bytes(8));
+                } catch (\Exception $e) {
+                    $captionId = 'table-caption-'.str_replace('.', '-', uniqid('', true));
+                }
+
+                $caption = $dom->createElement('caption', $captionContent);
+                $caption->setAttribute('id', $captionId);
+
+                // Link the table to its caption for accessibility
+                $div->setAttribute('aria-labelledby', $captionId);
+
+                $table->insertBefore($caption, $table->firstChild);
+
+                // Remove the original <figcaption>
+                $figcaption->parentNode->removeChild($figcaption);
+            }
+
+            // Replace the <figure> wrapping the <table> with <div role="region" tabindex="0" class="table-wrap">
+            $figure->parentNode->replaceChild($div, $figure);
+
+            // move the <table> inside the <div>
+            if ($table) {
+                $div->appendChild($table);
+            }
+        }
+
+        // Serialize only the content inside our temporary root
+        $out = '';
+        foreach (iterator_to_array($root->childNodes) as $child) {
+            $out .= $dom->saveHTML($child);
+        }
+
+        return $out;
+    }
 }
