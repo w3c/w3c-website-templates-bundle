f16ef5836a7e5dc0763f16476c7057fe74c1469b
Jean-Guilhem Rouel <jean-gui@w3.org>
2026-04-02 15:19:40 +0200

    Moved two Twig filters to the frontend. These filters are only useful on the frontend when processing content from Craft.

--- src/Twig/TwigExtension.php
+++ src/Twig/TwigExtension.php
@@ -74,8 +74,6 @@ class TwigExtension extends AbstractExtension
             new TwigFilter('locale_to_bcp47', [$this, 'localeToBcp47']),
             new TwigFilter('bcp47_to_locale', [$this, 'bcp47ToLocale']),
             new TwigFilter('obfuscate', [$this, 'obfuscate'], ['is_safe' => ['html']]),
-            new TwigFilter('i_to_em', [$this, 'iToEm']),
-            new TwigFilter('formatTable', [$this, 'formatTable']),
         ];
     }
 
@@ -300,92 +298,5 @@ class TwigExtension extends AbstractExtension
     {
         // Obfuscate string by replacing all characters with their HTML entity equivalents
         return mb_encode_numericentity($text, [0x000000, 0x10ffff, 0, 0xffffff], 'UTF-8');
-    }
-
-    public function iToEm($text)
-    {
-        // Replace <i> with <em>
-        return preg_replace([
-            '/<i>/i',
-            '/<\/i>/i'
-        ], [
-            '<em>',
-            '</em>'
-        ], $text);
-    }
-
-    public function formatTable($content)
-    {
-        $html5 = new HTML5();
-
-        // Wrap the incoming snippet so we can safely query & replace
-        $dom =  $html5->loadHTML(
-            '<!DOCTYPE html><meta charset="utf-8"><div id="__root__">'.$content.'</div>'
-        );
-
-        // Find our temporary root
-        $xpath = new \DOMXPath($dom);
-        $root  = $xpath->query('//*[@id="__root__"]')->item(0);
-
-        // Collect <figure> nodes into an array (DOMNodeList is live)
-        $figures = [];
-        foreach ($dom->getElementsByTagName('figure') as $figure) {
-            $figures[] = $figure;
-        }
-
-        foreach ($figures as $figure) {
-            $table = $figure->getElementsByTagName('table')->item(0);
-
-            if (!$table) {
-                continue; // Skip if there's no <table> in the <figure>
-            }
-
-            // Create a <div> with the required attributes to wrap the <table>
-            $div = $dom->createElement('div');
-            $div->setAttribute('role', 'region');
-            $div->setAttribute('tabindex', '0');
-            $div->setAttribute('class', 'table-wrap');
-
-            // If a caption has been entered, move this out of <figcaption> (which can be removed) and into <caption>, which should be the first child element of <table>
-            // Also set a globally-unique id on <caption>
-            $figcaption = $figure->getElementsByTagName('figcaption')->item(0);
-            if ($figcaption) {
-                $captionContent = $figcaption->textContent;
-
-                // Generate globally unique strong ID, with safe fallback
-                try {
-                    $captionId = 'table-caption-'.bin2hex(random_bytes(8));
-                } catch (\Exception $e) {
-                    $captionId = 'table-caption-'.str_replace('.', '-', uniqid('', true));
-                }
-
-                $caption = $dom->createElement('caption', $captionContent);
-                $caption->setAttribute('id', $captionId);
-
-                // Link the table to its caption for accessibility
-                $div->setAttribute('aria-labelledby', $captionId);
-
-                $table->insertBefore($caption, $table->firstChild);
-
-                // Remove the original <figcaption>
-                $figcaption->parentNode->removeChild($figcaption);
-            }
-
-            // Replace the <figure> wrapping the <table> with <div role="region" tabindex="0" class="table-wrap">
-            $figure->parentNode->replaceChild($div, $figure);
-
-            // move the <table> inside the <div>
-            if ($table) {
-                $div->appendChild($table);
-            }
-        }
-
-        // Serialize only the content inside our temporary root
-        $out = '';
-        foreach (iterator_to_array($root->childNodes) as $child) {
-            $out .= $dom->saveHTML($child);
-        }
-
-        return $out;
     }
 }
